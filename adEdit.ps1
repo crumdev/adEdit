aceccceee3e649844ce450279713d90563ae9107
Nathan Crum <Nathan.Crum@outlook.com>
2016-07-22 09:09:26 -0400

    Going to starting adding parameters to the script and functions to make it more flexible.

--- adEdit.ps1
+++ adEdit.ps1
@@ -4,23 +4,17 @@ $secPass = ConvertTo-SecureString "S@rah072016" -AsPlainText -Force;
 $mycreds = New-Object System.Management.Automation.PSCredential("20453065",$secPass);
 $domain = "xrxss.com";
 
+$csv = Import-Csv -path .\accounts.csv
 
 
-$data = @(
-    @{'id' = "30064038" },
-    @{'id' = "30203603" },
-    @{'id' = "50010860" },
-    @{'id' = "40846312" }
-)
+foreach($user in $csv) {
+    getUser($user);
+}
 
-foreach($user in $data) {
-    <#
-    # Uncomment the two below commands to test your file and that the accounts exist
-    #>
-    
-    #Write-Host $user.id
+function getUser ($user) {
     Get-ADUser $user.id -server $domain -credential $mycreds
-    
+}
 
-    #Disable-ADAccount $user.id -Server $domain -Credential $mycreds
+function enableUser ($user) {
+    Disable-ADAccount $user.id -server $domain -Credential $mycreds
 }
