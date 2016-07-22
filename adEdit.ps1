bbb6468976d2c61dfbf305787f361a147fe33114
Nathan Crum <Nathan.Crum@outlook.com>
2016-07-22 09:43:36 -0400

    Adding help text and moving to github

--- adEdit.ps1
+++ adEdit.ps1
@@ -1,20 +1,12 @@
-﻿Import-Module ActiveDirectory
+﻿Param ( 
+    [string]$adminUser = $env:username, 
+    [string]$adminPass, 
+    [string]$csv, 
+    [switch]$h = $false
+)
 
-$secPass = ConvertTo-SecureString "S@rah072016" -AsPlainText -Force;
-$mycreds = New-Object System.Management.Automation.PSCredential("20453065",$secPass);
-$domain = "xrxss.com";
-
-$csv = Import-Csv -path .\accounts.csv
-
-
-foreach($user in $csv) {
-    getUser($user);
-}
-
-function getUser ($user) {
-    Get-ADUser $user.id -server $domain -credential $mycreds
-}
-
-function enableUser ($user) {
-    Disable-ADAccount $user.id -server $domain -Credential $mycreds
+if ($h) {
+Write-Host("Help Text")
+} else {
+    Write-Host("Not working")
 }
